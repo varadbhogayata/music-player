02f9c9f54e127e1f7728c606be6b671ca2b88bf2
Varad Bhogayata <vbhogayata@gmail.com>
2020-07-08 00:03:50 +0530

    change db to postgreSQL, add settings dir

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicplayer.settings')
+    # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicplayer.settings1.developement')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
