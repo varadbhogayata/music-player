9f908e67d97042ac3eef80c2241d5cc79a018d28
Prerak Raja <rajaprerak@gmail.com>
2020-07-08 13:40:52 +0530

    song table added

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    genre = models.CharField(max_length=100)
+    song_img = models.FileField()
+    year = models.IntegerField()
+    singer = models.CharField(max_length=200)
+    song_file = models.FileField()
+
+    def __str__(self):
+        return self.name
--- musicplayer/urls.py
+++ musicplayer/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,3 +24,7 @@ urlpatterns = [
     path('', include('musicapp.urls')),
     path('authentication/', include('authentication.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
