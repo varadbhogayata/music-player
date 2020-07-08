d658a44575d8b82705ef43bb3d6e1a4a67cb72e1
Prerak Raja <rajaprerak@gmail.com>
2020-07-08 13:54:04 +0530

    search functionality completed

--- musicapp/views.py
+++ musicapp/views.py
@@ -4,11 +4,19 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import Song
+from django.db.models import Q
 
 
 # Create your views here.
 def index(request):
     songs = Song.objects.all()
+    query = request.GET.get('q')
+
+    if query:
+        songs = Song.objects.filter(Q(name__icontains=query)).distinct()
+        context = {'songs':songs}
+        return render(request, 'musicapp/index.html', context)
+
     context = {'songs':songs}
     return render(request, 'musicapp/index.html', context=context)
 
